cacf3743cdde9465d053f027fe8b4650024bdc31
s.isaev <v.isaev@kingbird.ru>
2018-04-20 13:58:29 +0300

    setting fix

--- TestAPI/settings.py
+++ TestAPI/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'API',
-    'rest_framework'
+    'rest_framework',
+    'django_heroku'
 ]
 
 MIDDLEWARE = [
