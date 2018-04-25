99780116b15988f18191046fe399dadbdf8f6650
s.isaev <v.isaev@kingbird.ru>
2018-04-25 09:38:47 +0300

    cor

--- API/serializers.py
+++ API/serializers.py
@@ -12,3 +12,10 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ('name', 'courses', 'id','url')
+    
+    def create(self, validated_data):
+        courses_data = validated_data.pop('courses')
+        student = Student.objects.create(**validated_data)
+        for course_data in courses_data:
+            Course.objects.create(student=student, **course_data)
+        return student
